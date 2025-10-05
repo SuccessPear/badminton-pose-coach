8a9f1a8904e0608da1eb593ae47de075b5ab9058
SuccessPear <lethanhcong3920@gmail.com>
2025-10-05 16:03:10 +0200

    Refactor code to add 02 dataset preprocessing pipeline later

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from badmintonPoseCoach import logger
 from badmintonPoseCoach.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-from badmintonPoseCoach.pipeline.stage_02_prepare_base_model import PrepareBaseModelPipeline
-from badmintonPoseCoach.pipeline.stage_03_model_training import TrainingModelPipeline
+from badmintonPoseCoach.pipeline.stage_03_prepare_base_model import PrepareBaseModelPipeline
+from badmintonPoseCoach.pipeline.stage_04_model_training import TrainingModelPipeline
 
 import multiprocessing as mp
 import platform
@@ -18,8 +18,8 @@ def run_stage(stage_name: str, PipelineCls):
 
 def main():
     run_stage("Data Ingestion", DataIngestionTrainingPipeline)
-    run_stage("Prepare Base Model", PrepareBaseModelPipeline)
-    run_stage("Training Model", TrainingModelPipeline)
+    #run_stage("Prepare Base Model", PrepareBaseModelPipeline)
+    #run_stage("Training Model", TrainingModelPipeline)
 
 if __name__ == "__main__":
     mp.freeze_support()
